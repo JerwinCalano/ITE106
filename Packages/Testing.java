f8ad6491426cd2283ea4f8da2b164fdf36f2f57a
JerwinCalano <calanojerwin08@gmail.com>
2024-11-04 15:45:57 +0800

    Create Testing.java

---
+++ Packages/Testing.java
@@ -0,0 +1,11 @@
+import package_one.ClassTwo;
+import package_one.ClassOwo;
+
+public class Testing {
+	pulic static void main (String[] args){
+		ClassTwo a = new ClassTwo();
+		ClassOne a = new ClassOne();
+		a.methodClassTwo;
+		a.methodClassOne;
+	}
+}
